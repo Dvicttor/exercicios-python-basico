8ae9e1770d8089ebd0b2c350364ad2a0c6c200ff
victor campos <victtor2210@gmail.com>
2026-03-23 10:13:44 -0400

    novo exercicio

---
+++ 4aula/exercicio1.py
@@ -0,0 +1,8 @@
+faltas1 = int(input("Digite o número de faltas do 1º bimestre: "))
+faltas2 = int(input("Digite o número de faltas do 2º bimestre: "))
+
+# Cálculo da soma
+total_faltas = faltas1 + faltas2
+
+# Saída
+print("Total de faltas =", total_faltas)
